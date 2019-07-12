412be2bfad577a4202f77cf1015fbf8c953036b1
Pedrohrn <dev14.seucondominio@gmail.com>
2019-07-12 19:54:41 -0300

    liberação de rotas

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,17 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'usuarios#index'
 
-  resources :grupos, 		only: [:index]
-  resources :cargos, 		only: [:index]
-  resources :usuarios, 	only: [:index]
+  resources :grupos, 		only: [:index, :create, :update, :destroy, :show] do
+  	collection do
+  		put :micro_update
+  	end
+  end
+  resources :cargos, 		only: [:index, :create, :update, :destroy, :show] do
+  	collection do
+  		put :micro_update
+  	end
+  end
+  resources :recessos,  only: [:index, :create, :update, :destroy, :show]
+  resources :usuarios, 	only: [:index, :show]
 end
 
