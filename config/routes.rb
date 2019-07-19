0e0c95fe13fe0c3b0ab6c0a488c86741376fae98
Pedrohrn <dev14.seucondominio@gmail.com>
2019-07-19 15:07:49 -0300

    liberando o método/rota :submit, que substitui os create e update

--- config/routes.rb
+++ config/routes.rb
@@ -2,22 +2,29 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'usuarios#index'
 
-  resources :recessos,  only: [:index, :create, :update, :destroy, :show]
-
-  resources :grupos, 		only: [:index, :create, :update, :destroy, :show] do
-  	collection do
-  		put :micro_update
-  	end
-  end
-
-  resources :cargos, 		only: [:index, :create, :update, :destroy, :show] do
-  	collection do
-  		put :micro_update
-  	end
-  end
-
-  resources :usuarios, 	only: [:index, :show, :create, :update, :destroy] do
+  resources :recessos,  only: [:index, :destroy, :show] do
     collection do
+      post :submit
+    end
+  end
+
+  resources :grupos, 		only: [:index, :destroy, :show] do
+  	collection do
+      post :submit
+  		put :micro_update
+  	end
+  end
+
+  resources :cargos, 		only: [:index, :destroy, :show] do
+  	collection do
+      post :submit
+  		put :micro_update
+  	end
+  end
+
+  resources :usuarios, 	only: [:index, :show, :destroy] do
+    collection do
+      post :submit
       put :micro_update
     end
   end
