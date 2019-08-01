66c758aac628e9a615198d2f8eab680a0b876789
Pedrohrn <dev14.seucondominio@gmail.com>
2019-08-01 16:08:43 -0300

    modal de grupo não é mais utilizado. alterações nos nomes das colunas desfeitos.

--- db/migrate/20190731132713_change_column_name.rb
+++
@@ -1,6 +0,0 @@
-class ChangeColumnName < ActiveRecord::Migration[5.2]
-  def change
-  	rename_column :administrativo_funcionarios, :grupo_id, :funcionario_grupo_id
-  	rename_column :administrativo_funcionarios, :cargo_id, :funcionario_cargo_id
-  end
-end
